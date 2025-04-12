ef11fcb3f7fe0057cd913601161f6f7364296720
hilkiasmoraes <hilkias.moraes@gmail.com>
2025-04-12 11:19:12 -0300

    feat: Class Professor

--- src/main/java/com/escola/senai/model/Professor.java
+++ src/main/java/com/escola/senai/model/Professor.java
@@ -1,4 +1,37 @@
 package com.escola.senai.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    public void setDisciplinaProfessor(String disciplinaProfessor) {
+        this.disciplinaProfessor = disciplinaProfessor;
+    }
 }
