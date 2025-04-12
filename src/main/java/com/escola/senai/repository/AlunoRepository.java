1b0d8f8893ee0285978263dafc34d25182928800
hilkiasmoraes <hilkias.moraes@gmail.com>
2025-04-12 11:38:44 -0300

    feat: Repository

--- src/main/java/com/escola/senai/repository/AlunoRepository.java
+++ src/main/java/com/escola/senai/repository/AlunoRepository.java
@@ -1,4 +1,7 @@
 package com.escola.senai.repository;
 
-public class AlunoRepository {
+import com.escola.senai.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository <Aluno, Long> {
 }
--- src/main/java/com/escola/senai/repository/ProfessorRepository.java
+++ src/main/java/com/escola/senai/repository/ProfessorRepository.java
@@ -1,4 +1,7 @@
 package com.escola.senai.repository;
 
-public interface ProfessorRepository {
+import com.escola.senai.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository <Professor, Long> {
 }
